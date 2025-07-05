6b8c3fbf95d60c17b13d3ce5b28a83f823746d0a
Bart Massey <bart.massey@gmail.com>
2025-07-05 16:49:54 -0700

    started dumb solver

--- src/main.rs
+++ src/main.rs
@@ -6,28 +6,65 @@ mod words;
 use squares::*;
 use words::*;
 
+use std::collections::HashSet;
 use std::io::{BufReader, BufRead};
 use std::fs::File;
 use std::process::exit;
 
 use anyhow::Error;
 
-fn run() -> Result<(), Error> {
+type WordSet = HashSet<Word>;
+
+fn find_first(s: &mut Square, words: &[Word], used: &mut WordSet) -> Option<Square> {
+    if s.is_full() {
+        return Some(s.clone());
+    }
+
+    for &w in words {
+        if used.contains(&w) {
+            continue;
+        }
+        let range = if s.get_char(0, 0) == '.' {
+            0..1
+        } else {
+            1..10
+        };
+        for p in range {
+            if s.is_fit(p, w) {
+                let undo = s.get_pos(p);
+                s.set_pos(p, w);
+                used.insert(w);
+                if let Some(s) = find_first(s, words, used) {
+                    return Some(s);
+                }
+                used.remove(&w);
+                s.set_pos(p, undo);
+            }
+        }
+    }
+
+    None
+}
+
+fn run() -> Result<Option<Square>, Error> {
     let words = File::open("usa_5.txt")?;
     let words = BufReader::new(words)
         .lines()
         .map(|w| Word::from_str(&w?))
         .collect::<Result<Vec<_>, Error>>()?;
-    // println!("{:?}", words);
-    for w in words {
-        println!("{}", w.as_string());
-    }
-    Ok(())
+
+    let mut s = Square::default();
+    let mut used = HashSet::with_capacity(25);
+    Ok(find_first(&mut s, &words, &mut used))
 }
 
 fn main() {
-    if let Err(e) = run() {
-        eprintln!("dms: {}", e);
-        exit(1);
+    match run() {
+        Err(e) => {
+            eprintln!("dms: {}", e);
+            exit(1);
+        }
+        Ok(Some(s)) => println!("{}", s.as_string()),
+        Ok(None) => println!("no squares"),
     }
 }
