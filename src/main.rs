77af0197315d81186de1a809783261369f5da746
Bart Massey <bart.massey@gmail.com>
2025-07-04 21:47:55 -0700

    doubly-magic word squares

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+#![allow(clippy::uninlined_format_args)]
+
+mod squares;
+use squares::*;
+
+use std::io::{BufReader, BufRead};
+use std::fs::File;
+use std::process::exit;
+
+use anyhow::Error;
+
+fn run() -> Result<(), Error> {
+    let words = File::open("usa_5.txt")?;
+    let words = BufReader::new(words)
+        .lines()
+        .map(|w| Word::from_str(&w?))
+        .collect::<Result<Vec<_>, Error>>()?;
+    // println!("{:?}", words);
+    for w in words {
+        println!("{}", w.as_string());
+    }
+    Ok(())
+}
+
+fn main() {
+    if let Err(e) = run() {
+        eprintln!("dms: {}", e);
+        exit(1);
+    }
+}
